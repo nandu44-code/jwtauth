85d8e88da0dbee75b9ad754ecabc7d0bbaaa58b7
nandu44-code <nanduganeshnair4444@gmail.com>
2024-01-31 16:42:28 +0530

    password is hashed in serializer

--- jwtauth/urls.py
+++ jwtauth/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from . import users
+import users.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- users/views.py
+++ users/views.py
@@ -1,7 +1,12 @@
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer
 # Create your views here.
 
 class Register(APIView):
 
     def post(self,request):
-        pass
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
