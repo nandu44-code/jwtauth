64f65f16dc0f7e87e5494e9c4816ac7d9181ed5b
nandu44-code <nanduganeshnair4444@gmail.com>
2024-02-01 10:18:46 +0530

    Autherisation for users implimented

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . views import Register,Login
+from . views import Register,Login,UserView
 
 urlpatterns = [
     path('register',Register.as_view()),
-    path('login',Login.as_view())
-
+    path('login',Login.as_view()),
+    path('user',UserView.as_view())
 ]
--- users/views.py
+++ users/views.py
@@ -45,3 +45,22 @@ class Login(APIView):
         }
 
         return response
+    
+
+class UserView(APIView):
+    def get(self,request):
+        token =request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed('Unauthenticated!')
+        
+        try:
+            payload = jwt.decode(token, 'secret',algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Unautheticated!')
+        
+        user = User.objects.filter(id=payload['id']).first()
+        print(user.email)
+        serializer = UserSerializer(user)
+
+        return Response(serializer.data)
